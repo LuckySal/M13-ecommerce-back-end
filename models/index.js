3132e593b6302cb37328ec6b5100aff38196e87a
Brandon <hillbp2014@gmail.com>
2022-11-07 18:04:21 -0500

    Created all models and associations

--- models/index.js
+++ models/index.js
@@ -3,4 +3,8 @@ const Product = require("./Product");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-module.exports =  { Category, Product, Tag, ProductTag };
+Product.belongsToMany(Tag, { through: "product-tag" });
+
+Tag.belongsToMany(Product, { through: "product-tag" });
+
+module.exports = { Category, Product, Tag, ProductTag };
